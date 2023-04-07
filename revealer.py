c3d19a269e8af9a651c5a34af5dff9f5219d7250
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-04-07 11:31:31 +0200

    Set path fixing and export

--- revealer.py
+++ revealer.py
@@ -29,4 +29,35 @@ if not os.path.isdir(rdir):
 
 # --- Import template index.html
 
-tpl = rdir
+tfile = open(rdir + 'index.html', "r")
+out = tfile.read()
+
+# --- Path fixing
+
+rList = [
+  ('<link rel="stylesheet" href="', '<link rel="stylesheet" href="reveal.js/'),
+  ('<script src="', '<script src="reveal.js/'),
+]
+
+for old, new in rList:
+    out = out.replace(old, new)
+
+# --- Settings
+
+rList = [
+  ('monokai.css', 'zenburn.css'),
+]
+
+for old, new in rList:
+    out = out.replace(old, new)
+
+# --- Content --------------------------------------------------------------
+
+
+
+
+# --- Export ---------------------------------------------------------------
+
+ofile = pdir + os.path.splitext(os.path.basename(pfile))[0] + '.html'
+with open(ofile, "w") as fid:
+  fid.write(out)
