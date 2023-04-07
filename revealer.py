2d1b318cd3443656bf69b66a77491e4c2b62adcb
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-04-07 13:44:08 +0200

    Parsing and content injection

--- revealer.py
+++ revealer.py
@@ -24,6 +24,44 @@ if not os.path.isdir(rdir):
 
 # === Parsing ==============================================================
 
+slides = []
+with open(pfile, "r") as fid:
+  for line in fid:
+
+    # --- First slide
+
+    s = '>>> first: '
+    if line.startswith(s):
+      slides.append({'type': 'first', 'title': line[len(s):], 'html': ''})
+      continue
+
+    # --- Section slides
+
+    s = r'%%% '
+    if line.startswith(s):
+      slides.append({'type': 'section', 'title': line[len(s):], 'html': ''})
+      continue
+
+    # --- Regular slide
+
+    s = '=== '
+    if line.startswith(s):
+      slides.append({'type': 'slide', 'title': line[len(s):], 'html': ''})
+      continue
+
+    # --- Children slides
+
+    s = '--- '
+    if line.startswith(s):
+      slides[-1]['type'] = 'parent'
+      slides.append({'type': 'children', 'title': line[len(s):], 'html': ''})
+      continue
+
+    # --- Slide content
+
+    if len(slides):
+      slides[-1]['html'] += line
+
 
 # === Output ===============================================================
 
@@ -53,8 +91,27 @@ for old, new in rList:
 
 # --- Content --------------------------------------------------------------
 
+# --- Build content
 
+content = ''
+for S in slides:
+   
+  if S['type'] == 'parent':
+     content += '<section>'
 
+  content += '<section>'
+  content += '<h1>' + S['title']+ '</h1>'
+  content += S['html']
+  content += '</section>'
+
+  if S['type'] == 'children':
+     content += '</section>'
+
+# --- Injects into html
+
+s = '<div class="slides">\n'
+i = out.find(s) + len(s)
+out = out[0:i] + content + out[i:]
 
 # --- Export ---------------------------------------------------------------
 
