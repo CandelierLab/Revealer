a18b3937273e34166006998797a8154943a32b35
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-04-07 10:51:16 +0200

    Automatic copy of reveal.js

--- revealer.py
+++ revealer.py
@@ -1,1 +1,32 @@
-print('ok')
+import sys
+import os
+
+# === Settings =============================================================
+
+# Presentation file name
+try:
+  pfile = sys.argv[1]
+except:
+  pfile = '/home/raphael/Science/Presentations/Test/Presentation.pres'
+
+# Presentation path
+pdir = os.path.dirname(pfile)+'/'
+
+# === Reveal.js ============================================================
+
+# Check existence
+rdir = pdir + 'reveal.js/'
+if not os.path.isdir(rdir):
+
+  # Copy folder
+  import shutil
+  shutil.copytree(os.getcwd()+'/reveal.js', rdir)
+
+# === Parsing ==============================================================
+
+
+# === Output ===============================================================
+
+# --- Import template index.html
+
+tpl = rdir
