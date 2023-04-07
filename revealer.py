3c0e1d00d6d91ca3117cea17d3548bec1c90906b
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-04-07 15:48:55 +0200

    Improve styling (header)

--- revealer.py
+++ revealer.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import shutil
+import re
 
 # === Settings =============================================================
 
@@ -19,12 +21,18 @@ rdir = pdir + 'reveal.js/'
 if not os.path.isdir(rdir):
 
   # Copy folder
-  import shutil
   shutil.copytree(os.getcwd()+'/reveal.js', rdir)
+
+# Add custom themes
+themes = os.listdir(os.getcwd()+'/theme')
+for theme in themes:
+  shutil.copyfile(os.getcwd()+'/theme/'+theme, rdir+'dist/theme/'+theme)
 
 # === Parsing ==============================================================
 
-slides = []
+setting = {}
+slide = []
+
 with open(pfile, "r") as fid:
   for line in fid:
 
@@ -32,36 +40,47 @@ with open(pfile, "r") as fid:
 
     s = '>>> first: '
     if line.startswith(s):
-      slides.append({'type': 'first', 'title': line[len(s):], 'html': ''})
+      slide.append({'type': 'first', 'title': line[len(s):].strip(), 'html': ''})
       continue
 
     # --- Section slides
 
     s = r'%%% '
     if line.startswith(s):
-      slides.append({'type': 'section', 'title': line[len(s):], 'html': ''})
+      slide.append({'type': 'section', 'title': line[len(s):].strip(), 'html': ''})
       continue
 
     # --- Regular slide
 
     s = '=== '
     if line.startswith(s):
-      slides.append({'type': 'slide', 'title': line[len(s):], 'html': ''})
+      slide.append({'type': 'slide', 'title': line[len(s):].strip(), 'html': ''})
       continue
 
     # --- Children slides
 
     s = '--- '
     if line.startswith(s):
-      slides[-1]['type'] = 'parent'
-      slides.append({'type': 'children', 'title': line[len(s):], 'html': ''})
+      slide[-1]['type'] = 'parent'
+      slide.append({'type': 'children', 'title': line[len(s):].strip(), 'html': ''})
       continue
+
+    # --- Settings
+    
+    if line.startswith('>'):
+      x = re.search('^> ([^:]*): (.*)', line)
+      if x:
+        if x.group(1) in setting:
+          if not isinstance(setting[x.group(1)], list):
+            setting[x.group(1)] = [setting[x.group(1)]]
+          setting[x.group(1)].append(x.group(2))
+        else:
+          setting[x.group(1)] = x.group(2)
 
     # --- Slide content
 
-    if len(slides):
-      slides[-1]['html'] += line
-
+    if len(slide):
+      slide[-1]['html'] += line
 
 # === Output ===============================================================
 
@@ -84,23 +103,37 @@ for old, new in rList:
 
 rList = [
   ('monokai.css', 'zenburn.css'),
+  ('theme/black.css', 'theme/{:s}.css'.format(setting['theme'])),
 ]
 
 for old, new in rList:
     out = out.replace(old, new)
+
+# --- Styling
+
+out = out.replace('<body>', '<body><header></header>')
 
 # --- Content --------------------------------------------------------------
 
 # --- Build content
 
 content = ''
-for S in slides:
+for k, S in enumerate(slide):
    
   if S['type'] == 'parent':
-     content += '<section>'
+    content += '<section data-transition="none">'
+     
+  content += '<section data-transition="none" data-state="slide_{:d}">'.format(k)
+  
+  match S['type']:
 
-  content += '<section>'
-  content += '<h1>' + S['title']+ '</h1>'
+    case 'first' | 'section':
+      content += '<style>.slide_{:d} header {{ display: none; }}</style>'.format(k)
+      content += '<h1>' + S['title']+ '</h1>'
+
+    case _:
+      content += '<style>.slide_{:d} header::after {{ content: "{:s}"; }}</style>'.format(k, S['title'])  
+
   content += S['html']
   content += '</section>'
 
@@ -118,3 +151,5 @@ out = out[0:i] + content + out[i:]
 ofile = pdir + os.path.splitext(os.path.basename(pfile))[0] + '.html'
 with open(ofile, "w") as fid:
   fid.write(out)
+
+print(slide)
